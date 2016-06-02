dbda22ab8cb4509507550660e6d213f42ce1a677
Christophe CHAUVET <christophe.chauvet@gmail.com>
2016-06-02 16:48:01 +0200

    [ADD] Add sphinx theme

---
+++ sphinx_adc_theme/__init__.py
@@ -0,0 +1,16 @@
+"""Sphinx ADC theme.
+
+Fork From https://github.com/coordt/ADCtheme/.
+
+"""
+import os
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(str(v) for v in VERSION)
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
